55909324a00b21e6e78cc9b05f70b5cca3bf0e5c
Prometheus <PrometheusSSE@outlook.com>
2022-04-03 15:29:37 +0200

    Project setup

--- src/main/java/com/prometheusmodding/ghiblimcmod/mixin/ExampleMixin.java
+++ src/main/java/com/prometheusmodding/ghiblimcmod/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.prometheusmodding.ghiblimcmod.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.prometheusmodding.ghiblimcmod.GhibliMCMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,8 +9,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
-	}
+
+  @Inject(at = @At("HEAD"), method = "init()V")
+  private void init(CallbackInfo info) {
+    GhibliMCMod.LOGGER.info("This line is printed by an example mod mixin!");
+  }
 }
